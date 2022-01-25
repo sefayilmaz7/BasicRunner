d776a188e96d8eb0d330ecdee6dc681f01b11f9b
Sefa <sefayilmazkurumsal@gmail.com>
2022-01-25 19:53:20 +0300

    Damage Effect

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
     [SerializeField] Vector3 offset;
+    [SerializeField] private GameObject damageEffect;
     private float staticPositionY = 2.49f;
+
 
     private void LateUpdate()
     {
@@ -12,5 +15,34 @@ public class CameraController : MonoBehaviour
         Vector3 smoothPosition = Vector3.Lerp(transform.position , newPos , 0.1f);
         smoothPosition.y = staticPositionY;
         gameObject.transform.position = smoothPosition;
+    }
+
+    private void OnEnable()
+    {
+        EventManager.onHitObstalce += TakeDamage;
+        EventManager.onGameFinished += SetCameraToFinishScene;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.onHitObstalce -= TakeDamage;
+        EventManager.onGameFinished -= SetCameraToFinishScene;
+    }
+
+    private IEnumerator ApplyDamageEffect() 
+    {
+        damageEffect.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        damageEffect.SetActive(false);
+    }
+
+    void TakeDamage() 
+    {
+        StartCoroutine(ApplyDamageEffect());
+    }
+
+    void SetCameraToFinishScene() 
+    {
+        // I'll edit camera movement for this
     }
 }
